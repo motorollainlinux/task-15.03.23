c929d210bba34361ab820d37e0dd87b57fed2ee7
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-03-28 20:42:06 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
-
-using namespace std;
+#include <math.h>
 
 class Person { 
-    public: int heigt, weight, age;
-    string name, text;
-    Person(int heigt, int weight, int age, string name) {
-        this.heigt = heigt;
-        this.weight = weight;
-        this.age = age;
-        this.name = name;
+    public:
+    int heigt, weight, age;
+    std::string name, text;
+    Person(int heigt, int weight, int age, std::string name) {
+        this -> heigt = heigt;
+        this -> weight = weight;
+        this -> age = age;
+        this -> name = name;
     }
-    void say(string text) {
-        cout << name << "сказал: " << text << "\n";
+    void say(std::string text) {
+        std::cout << name << "say: " << text << "\n";
+    }
+    void say(std::string text, std::string nameofdestinationer) {
+        std::cout << name << "say to " << nameofdestinationer << " :" << text << "\n";
     }
     void AgeMeneger(int days) {
-        
+        int ages = round(days/356);
+        age += ages;
     }
 };
 int main() {
-    Person newperson = new Person;
-    
+    Person newperson = new Person(176, 57, 16, "Kirill");
 }
