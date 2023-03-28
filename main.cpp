e3518174a2c24ec3cae011683eb538f85f59bd68
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-03-28 21:08:48 +0700

    comlete task and add 1 memes

--- main.cpp
+++ main.cpp
@@ -12,16 +12,20 @@ class Person {
         this -> name = name;
     }
     void say(std::string text) {
-        std::cout << name << "say: " << text << "\n";
+        std::cout << name << " say: \"" << text << "\"\n";
     }
     void say(std::string text, std::string nameofdestinationer) {
-        std::cout << name << "say to " << nameofdestinationer << " :" << text << "\n";
+        std::cout << name << " say to " << nameofdestinationer << ":\"" << text << "\"\n";
     }
     void AgeMeneger(int days) {
         int ages = round(days/356);
         age += ages;
+        std::cout << name << " has been grove on " << ages << " age. And him then " << age << " age.\n";
     }
 };
 int main() {
-    Person newperson = new Person(176, 57, 16, "Kirill");
+    Person* newperson = new Person(176, 57, 16, "Kirill");
+    newperson -> say("I wanna salami.");
+    newperson -> AgeMeneger(2000);
+    newperson -> say("You knoow the rules and so do I... Say goodbye!", "Rick Ahtley");
 }
